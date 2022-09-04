09cd597d8facb6cea92e9505d11546e618e57800
tutugodfrey <godfrey_tutu@yahoo.com>
2022-09-04 01:47:22 +0100

    Reverse message

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var app = require('../index.js');
 describe('GET /', function() {
   it('respond with hello world', function(done) {
     //navigate to root and check the the response is "hello world"
-    request(app).get('/').expect('Hello world!!', done);
+    request(app).get('/').expect('Hello world!', done);
   });
 });
 
